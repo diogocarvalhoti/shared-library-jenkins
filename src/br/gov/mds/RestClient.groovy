a4134ef43517b3c3c3246c1b792359da4d23a87d
Diogo Carvalho de Matos <diogo.matos@terceirizado.cidadania.gov.br>
2019-12-23 17:15:09 -0300

    merge_requests

--- src/br/gov/mds/RestClient.groovy
+++ src/br/gov/mds/RestClient.groovy
@@ -2,41 +2,38 @@ package br.gov.mds
 
 import com.cloudbees.groovy.cps.NonCPS
 import groovy.json.JsonSlurperClassic
+import groovy.transform.MapConstructor
 
 class RestClient {
-	
+
 	private String baseUrl;
 	private String token;
-	
+
 	public RestClient(baseUrl, token) {
 		this.baseUrl = baseUrl;
 		this.token = token;
 	}
-	
+
 	public Integer getIdProject(String namespace) {
 		Map resultMap = get(this.baseUrl.concat("/api/v4/projects/").concat(namespace))
 		return resultMap.get("id")
 	}
-	
-	public void createMR(url, token, sourceBranch, targetBranch) {
+
+	public void createMR(Integer idProject, String sourceBranch, String targetBranch) {
+		Map<String, String> params = new HashMap();
+		params.put("source_branch", sourceBranch);
+		params.put("target_branch", targetBranch);
+		params.put("title", "Teste MR");
 		
+		String uri = this.baseUrl.concat("/api/v4/projects/").concat(idProject).concat("merge_requests")
 		
+		post(uri, params)
 	}
-	
+
 	@NonCPS
 	private Map get(String uri) {
-		URL url = new URL(uri);
-		println this.token
-		println url
-		
-		HttpURLConnection connection = url.openConnection()
-		
-		println this.token
-		
-		if (this.token != null && this.token.length() > 0) {
-			connection.setRequestProperty("Private-Token", this.token)
-		}
-		
+		HttpURLConnection connection = new URL(uri).openConnection()
+		connection.setRequestProperty("Private-Token", this.token)
 		connection.setRequestMethod("GET")
 		connection.setDoInput(true)
 		connection.setDoOutput(true)
@@ -51,5 +48,62 @@ class RestClient {
 		}
 		return rs
 	}
-	
+
+	@NonCPS
+	private String post(String uri, Map params) {
+		String response = "";
+		HttpURLConnection connection = new URL(uri).openConnection();
+		connection.setReadTimeout(15000);
+		connection.setConnectTimeout(15000);
+		connection.setRequestMethod("POST");
+		connection.setDoInput(true);
+		connection.setDoOutput(true);
+
+		try {
+			OutputStream os = connection.getOutputStream();
+			BufferedWriter writer = new BufferedWriter(
+					new OutputStreamWriter(os, "UTF-8"));
+
+
+			writer.write(getPostDataString(params));
+
+			writer.flush();
+			writer.close();
+			os.close();
+			int responseCode = connection.getResponseCode();
+
+			if (responseCode == HttpsURLConnection.HTTP_OK) {
+				String line;
+				BufferedReader br=new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				while ((line=br.readLine()) != null) {
+					response+=line;
+				}
+			}
+			else {
+				response="";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			connection.disconnect();
+		}
+		return response;
+	}
+
+	private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException{
+		StringBuilder result = new StringBuilder();
+		boolean first = true;
+		for(Map.Entry<String, String> entry : params.entrySet()){
+			if (first)
+				first = false;
+			else
+				result.append("&");
+
+			result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+			result.append("=");
+			result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+		}
+
+		return result.toString();
+	}
 }
