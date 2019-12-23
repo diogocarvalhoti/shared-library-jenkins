afd29d79b7f69e8a28b326b8cc7eec296e021af5
Diogo Carvalho de Matos <diogo.matos@terceirizado.cidadania.gov.br>
2019-12-23 15:09:16 -0300

    Versionando arquivo inicial

---
+++ vars/restGetURL.groovy
@@ -0,0 +1,39 @@
+#! /home/diogo/.sdkman/candidates/groovy/2.5.8/bin/groovy
+
+import groovy.json.JsonSlurperClassic
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    if (config.url == null) {
+        error "No URL found"
+    }
+
+    retry(3) {
+        return getResult(config.url, config.token)
+    }
+}
+
+def getResult(url, token) {
+    echo "${url}"
+	
+    HttpURLConnection connection = url.openConnection()
+    if (token != null && token.length() > 0) {
+        connection.setRequestProperty("Private-Token:", "${token}")
+    }
+    connection.setRequestMethod("GET")
+    connection.setDoInput(true)
+    def rs = null
+    try {
+        connection.connect()
+        rs = new JsonSlurperClassic().parse(new InputStreamReader(connection.getInputStream(), "UTF-8"))
+    } finally {
+        connection.disconnect()
+    }
+    echo 'returning'
+    return rs
+}
