1b1a43ca976a2384f77036d0e7e19b12de666d6c
Diogo Carvalho de Matos <diogo.matos@terceirizado.cidadania.gov.br>
2019-12-23 17:56:53 -0300

    asd

--- vars/restClient.groovy
+++ vars/restClient.groovy
@@ -1,38 +1,112 @@
-#! /home/diogo/.sdkman/candidates/groovy/2.5.8/bin/groovy
-
-import groovy.json.JsonSlurperClassic
 import com.cloudbees.groovy.cps.NonCPS
+import groovy.json.JsonSlurperClassic
+
+public class RestClient {
+
+	private String baseUrl;
+	private String token;
 
 
+	public RestClient(baseUrl, token) {
+		println("TESTEEEE")
+
+		this.baseUrl = baseUrl;
+		this.token = token;
+	}
+
+	public Integer getIdProject(String namespace) {
+		Map resultMap = get(this.baseUrl.concat("/api/v4/projects/").concat(namespace))
+		return resultMap.get("id")
+	}
+
+	public String createMR(Integer idProject, String sourceBranch, String targetBranch) {
+		Map<String, String> params = new HashMap();
+		params.put("source_branch", sourceBranch);
+		params.put("target_branch", targetBranch);
+		params.put("title", "Teste MR");
+
+		String uri = this.baseUrl.concat("/api/v4/projects/").concat(idProject.toString()).concat("merge_requests")
+
+		return post(uri, params)
+	}
+
+	@NonCPS
+	private Map get(String uri) {
+		HttpURLConnection connection = new URL(uri).openConnection()
+		connection.setRequestProperty("Private-Token", this.token)
+		connection.setRequestMethod("GET")
+		connection.setDoInput(true)
+		connection.setDoOutput(true)
+		def rs = null
+		try {
+			connection.connect()
+			def responseCode = connection.getResponseCode()
+			println "Response Code: "+responseCode
+			rs = new JsonSlurperClassic().parse(new InputStreamReader(connection.getInputStream(), "UTF-8"))
+		} finally {
+			connection.disconnect()
+		}
+		return rs
+	}
+
+	@NonCPS
+	private String post(String uri, Map params) {
+		println "Executando POST: " + uri
+
+		String response = "";
+		try {
+			HttpURLConnection connection = new URL(uri).openConnection();
+			connection.setReadTimeout(15000);
+			connection.setConnectTimeout(15000);
+			connection.setRequestMethod("POST");
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
 
 
-def getIdProject(url, token) {
-	return resultMap.get("id")
-}
+			OutputStream os = connection.getOutputStream();
+			BufferedWriter writer = new BufferedWriter(
+					new OutputStreamWriter(os, "UTF-8"));
 
-def createMR(url, token, sourceBranch, targetBranch) {
-	
-}
+			writer.write(getPostDataString(params));
 
-@NonCPS
-def getResult(url, token) {
-    HttpURLConnection connection = url.openConnection()
-	
-    if (token != null && token.length() > 0) {
-        connection.setRequestProperty("Private-Token", "${token}")
-    }
-	
-    connection.setRequestMethod("GET")
-    connection.setDoInput(true)
-	connection.setDoOutput(true)
-    def rs = null
-    try {
-        connection.connect()
-		def responseCode = connection.getResponseCode()
-		println "Response Code: "+responseCode
-        rs = new JsonSlurperClassic().parse(new InputStreamReader(connection.getInputStream(), "UTF-8"))
-    } finally {
-        connection.disconnect()
-    }
-    return rs
+			writer.flush();
+			writer.close();
+			os.close();
+			int responseCode = connection.getResponseCode();
+
+			println "Response Code: " + responseCode
+
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				String line;
+				BufferedReader br=new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				while ((line=br.readLine()) != null) {
+					response+=line;
+				}
+			}
+			else {
+				response="";
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return response;
+	}
+
+	@NonCPS
+	private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException{
+		StringBuilder result = new StringBuilder();
+		boolean first = true;
+		for(Map.Entry<String, String> entry : params.entrySet()){
+			if (first)
+				first = false;
+			else
+				result.append("&");
+
+			result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+			result.append("=");
+			result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+		}
+
+		return result.toString();
+	}
 }
