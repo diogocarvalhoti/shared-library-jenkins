7df93a348e32f8e8973a142991a15acd3303134d
Diogo Carvalho de Matos <diogo.matos@terceirizado.cidadania.gov.br>
2019-12-26 13:46:53 -0300

    Ajustando Flow

--- src/br/gov/mds/GitFlow.groovy
+++ src/br/gov/mds/GitFlow.groovy
@@ -1,6 +1,35 @@
 package br.gov.mds
 
 public class GitFlow {
-	
-	
+
+	private String baseUrl;
+	private String token;
+	private RestClient client;
+
+	public GitFlow(baseUrl, token) {
+		this.baseUrl = baseUrl;
+		this.token = token;
+		
+		Logger.log("TESTEEE");
+	}
+
+	public Integer getIdProject(String namespace) {
+		String uri = new StringBuilder(this.baseUrl)
+				.append("/api/v4/projects/").append(namespace).toString()
+		Map resultMap = this.client.get(uri)
+		return resultMap.get("id")
+	}
+
+	public String createMR(Integer idProject, String sourceBranch) {
+		Map<String, String> params = new HashMap();
+		params.put("remove_source_branch", "true");
+		params.put("source_branch", sourceBranch);
+		params.put("target_branch", "develop");
+		params.put("title", "MR da branch: " + sourceBranch);
+
+		String uri = new StringBuilder(this.baseUrl)
+				.append("/api/v4/projects/").append(idProject).append("/merge_requests").toString();
+
+		return this.client.post(uri, params)
+	}
 }
--- src/br/gov/mds/RestClient.groovy
+++ src/br/gov/mds/RestClient.groovy
@@ -1,40 +1,11 @@
-package br.gov.mds
+package br.gov.mds;
 
 import com.cloudbees.groovy.cps.NonCPS
 
 import groovy.json.JsonSlurperClassic
 
-public class RestClient {
-
+public final class RestClient {
 	
-	private String baseUrl;
-	private String token;
-
-	public RestClient(baseUrl, token) {
-		this.baseUrl = baseUrl;
-		this.token = token;
-	}
-
-	public Integer getIdProject(String namespace) {
-		String uri = new StringBuilder(this.baseUrl)
-				.append("/api/v4/projects/").append(namespace).toString()
-		Map resultMap = get(uri)
-		return resultMap.get("id")
-	}
-
-	public String createMR(Integer idProject, String sourceBranch) {
-		Map<String, String> params = new HashMap();
-		params.put("remove_source_branch", "true");
-		params.put("source_branch", sourceBranch);
-		params.put("target_branch", "develop");
-		params.put("title", "MR da branch: " + sourceBranch);
-
-		String uri = new StringBuilder(this.baseUrl)
-				.append("/api/v4/projects/").append(idProject).append("/merge_requests").toString();
-
-		return post(uri, params)
-	}
-
 	@NonCPS
 	private Map get(String uri) {
 		HttpURLConnection connection = new URL(uri).openConnection()
